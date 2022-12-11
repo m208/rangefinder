4829e8a545dffedf28fe0c8c6520e4f4141798a5
m208 <avcvet@yandex.ru>
2022-12-11 19:15:50 +0300

    init: project settings

--- vue-rangefinder/src/main.ts
+++ vue-rangefinder/src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
+import router from "@/router/router";
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router).mount('#app');
+
