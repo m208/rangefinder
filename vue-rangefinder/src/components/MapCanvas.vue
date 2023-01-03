8fa58f4166aacc739dd21473d61c05fdcc437652
m208 <avkomissarov@gmail.com>
2023-01-03 22:31:55 +0300

    feat: add zooming methods

--- vue-rangefinder/src/components/MapCanvas.vue
+++ vue-rangefinder/src/components/MapCanvas.vue
@@ -1,18 +1,12 @@
 <script lang = 'ts'>
-import { zoomLevels } from "@/utils/utils"
-
 import pic from '@/assets/img/Erangel_Main_Low_Res.png';
 import picH from '@/assets/img/Erangel_Main_High_Res.jpg';
 import picM from '@/assets/img/Erangel_Main_Med.jpg';
 
-const MAX_ZOOM = 5;
-const MIN_ZOOM = 0.1;
-const SCROLL_SENSITIVITY = 0.0005;
-const clamp = (num: number, min: number, max: number) => Math.min(Math.max(num, min), max);
-
 declare interface BaseComponentData {
   currentMap: string;
   currentZoom: number;
+  prevZoom: number;
   wrapper: HTMLDivElement | null;
   canvas: HTMLCanvasElement | null;
   context: CanvasRenderingContext2D | null;
@@ -22,11 +16,15 @@ declare interface BaseComponentData {
   offset: ICoords;
   currentOffset: ICoords;
 }
+
 declare interface ICoords {
   x: number;
   y: number;
 }
+
 const emptyCoords: ICoords = { x: 0, y: 0 };
+
+const [MIN_ZOOM, MAX_ZOOM, ZOOM_STEP] = [0.25, 2, 0.25];
 
 export default {
   props: {
@@ -43,6 +41,7 @@ export default {
     return {
       currentMap: this.mapName,
       currentZoom: this.zoom,
+      prevZoom: this.zoom,
       wrapper: null,
       canvas: null,
       context: null,
@@ -55,9 +54,6 @@ export default {
   },
   onUpdated() { console.log('onUpdated'); },
   mounted() {
-    console.log('mounted');
-    console.log(zoomLevels);
-
     this.wrapper = this.$refs.canvaWrapperRef as HTMLDivElement;
     this.canvas = this.$refs.canvaRef as HTMLCanvasElement;
     this.context = this.canvas.getContext('2d');
@@ -67,89 +63,70 @@ export default {
 
     this.image.onload = () => {
       if (this.image !== null) {
-
         this.canvas!.width = this.wrapper!.offsetWidth;
         this.canvas!.height = this.wrapper!.offsetHeight;
-        //this.context!.scale(0.25, 0.25);
-        // const x = (this.canvas!.width / this.currentZoom - this.image!.width) / 2;
-        // const y = (this.canvas!.height / this.currentZoom - this.image!.height) / 2;
 
         this.setZoom();
         this.draw();
-        //this.context!.scale(this.currentZoom, this.currentZoom);
-        // this.context!.drawImage(
-        //   this.image, x, y
-        // );
       }
     }
   },
   methods: {
-    handleWheel(event: WheelEvent) {
-      const { deltaY } = event;
-      if (!this.dragging) {
-        this.currentZoom = this.currentZoom + deltaY * SCROLL_SENSITIVITY * -1, MIN_ZOOM, MAX_ZOOM
-        console.log(this.currentZoom);
-
-        this.setZoom();
-        this.draw();
-      }
-    },
-
     setZoom() {
-      const context = this.canvas?.getContext("2d")
+      const context = this.canvas?.getContext("2d");
+      context!.setTransform(1, 0, 0, 1, 0, 0);
       context!.scale(this.currentZoom, this.currentZoom);
     },
-    draw() {
-      const x = (this.canvas!.width / this.currentZoom - this.image!.width) / 2;
-      const y = (this.canvas!.height / this.currentZoom - this.image!.height) / 2;
-      this.context!.drawImage(
+
+    draw(centered = false) {
+      const canvas = this.$refs.canvaRef as HTMLCanvasElement;
+      const context = canvas?.getContext("2d")
+      context!.clearRect(0, 0, canvas!.width, canvas!.height);
+
+      const x = (canvas!.width / this.currentZoom - this.image!.width) / 2;
+      const y = (canvas!.height / this.currentZoom - this.image!.height) / 2;
+
+      const zoom = centered ? this.prevZoom : this.currentZoom;
+
+      context!.drawImage(
         this.image!,
-        x - this.currentOffset.x / this.currentZoom,
-        y - this.currentOffset.y / this.currentZoom,
+        x - this.currentOffset.x / zoom,
+        y - this.currentOffset.y / zoom,
       );
     },
 
+    handleZoom(direction: 'inc' | 'dec') {
+      const limit = direction === "inc" ? MAX_ZOOM : MIN_ZOOM;
+      if (this.currentZoom === limit) return;
+
+      this.prevZoom = this.currentZoom;
+      const delta = direction === "inc" ? ZOOM_STEP : -ZOOM_STEP;
+      this.currentZoom = +(this.currentZoom + delta).toFixed(2);
+      console.log(this.currentZoom);
+
+      this.setZoom();
+      this.draw(true);
+    },
+
     incZoom() {
-      // const zoomIndex = zoomLevels.findIndex(el => el === this.currentZoom);
-      // console.log('incZoom', zoomIndex);
-      //if (zoomIndex === -1 || zoomIndex === zoomLevels.length - 1) return;
-      //this.currentZoom = zoomLevels[zoomIndex + 1];
-      // console.log(this.currentZoom);
-      // this.currentZoom += 0.25
-      // this.context!.scale(1, 1);
-
-      if (this.currentZoom === 2) return // max
-      if (this.currentZoom < 1) {
-        this.currentZoom = 1.2;
-      } else this.currentZoom = +(this.currentZoom + 0.2).toFixed(2);
-      console.log(this.currentZoom);
-      this.setZoom();
-      this.draw();
+      this.handleZoom('inc');
     },
+
     decZoom() {
-      // const zoomIndex = zoomLevels.findIndex(el => el === this.currentZoom);
-      // console.log('decZoom', zoomIndex);
-      // //if (zoomIndex === -1 || zoomIndex === zoomLevels[0]) return;
-      // //this.currentZoom = zoomLevels[zoomIndex - 1];
-      // this.currentZoom -= 0.25
-      // this.context!.scale(1, 1);
-      if (this.currentZoom === 0.5) return // min
-      if (this.currentZoom > 1) {
-        this.currentZoom = 0.8
-      } else {
-        this.currentZoom = +(this.currentZoom - 0.1).toFixed(2);
-      }
-
-      console.log(this.currentZoom);
-      this.setZoom();
-      this.draw();
+      this.handleZoom('dec');
     },
+
+    handleWheel(event: WheelEvent) {
+      if (this.dragging) return;
+      const { deltaY } = event;
+      (deltaY > 0) ? this.incZoom() : this.decZoom();
+    },
+
     handleMouseDown(event: PointerEvent) {
+      if (event.button !== 0) return;
       this.touchStart.x = event.clientX;
       this.touchStart.y = event.clientY;
       this.dragging = true;
-      //console.log(this.touchStart);
-
     },
 
     handleMouseUp() {
@@ -160,16 +137,10 @@ export default {
 
     handleMouseMove(event: PointerEvent) {
       if (this.dragging) {
-        // const offset: ICoords = {
-        //   x: (this.touchStart.x - event.clientX),
-        //   y: (this.touchStart.y - event.clientY)
-        // }
 
         const currentOffset: ICoords = {
           x: this.offset.x + (this.touchStart.x - event.clientX),
           y: this.offset.y + (this.touchStart.y - event.clientY)
-          // x: (this.offset.x + offset.x),
-          // y: (this.offset.y + offset.y)
         }
 
         // if (currentOffset.x < 0 || currentOffset.x > this.image!.width - this.wrapper!.offsetWidth) {
@@ -181,26 +152,6 @@ export default {
 
         this.currentOffset = currentOffset;
         this.draw();
-        //console.log(currentOffset);
-
-        // Make sure we're zooming to the center
-        const x = (this.canvas!.width / this.currentZoom - this.image!.width) / 2;
-        const y = (this.canvas!.height / this.currentZoom - this.image!.height) / 2;
-
-
-        //this.context!.translate(currentOffset.x, currentOffset.y);
-        //this.context!.clearRect(0, 0, this.canvas!.width, this.canvas!.height);
-        //this.context!.scale(this.currentZoom, this.currentZoom);
-
-
-
-        // this.context!.drawImage(
-        //   //this.image!, x, y
-        //   this.image!, x - currentOffset.x / this.currentZoom, y - currentOffset.y / this.currentZoom
-        //   // this.image!, currentOffset.x / this.currentZoom, currentOffset.y / this.currentZoom, this.image!.width, this.image!.height, 0, 0, this.image!.width, this.image!.height,
-        //   // this.image!, currentOffset.x, currentOffset.y / this.currentZoom, this.image!.width, this.image!.height, 0, 0, this.image!.width, this.image!.height,
-        // );
-
       }
 
     },
