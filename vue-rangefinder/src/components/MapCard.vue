23c04e6495de4ecdd4a13241b182ddfc62d40916
m208 <avkomissarov@gmail.com>
2023-02-05 18:29:26 +0300

    feat: add cards on mainPage

---
+++ vue-rangefinder/src/components/MapCard.vue
@@ -0,0 +1,56 @@
+<script>
+export default {
+  props: {
+    mapName: {
+      type: String,
+      required: true,
+    },
+    mapImg: {
+      type: String,
+      required: true,
+    }
+  },
+  data() {
+    return {
+      thumb: this.mapImg,
+      name: this.mapName
+    }
+  }
+}
+</script>
+
+<template>
+<div class="card">
+  <div class="img__wrapper">
+    <img :src="thumb" :alt="name" class="card__img">
+  </div>
+  <div class="text_wrapper">
+    <p>{{ name }}</p>
+  </div>
+</div>
+</template>
+
+<style scoped>
+.card{
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  border-radius: 5px;
+  cursor: pointer;
+  transition-duration: 250ms;
+}
+.card:hover{
+  background-color: rgb(144 231 48);
+  transform: scale(1.1);
+}
+.card__img{
+  padding: 3px;
+  width: 250px;
+  height: 250px;
+}
+.text_wrapper{
+  margin-bottom: 5px;
+  text-align: center;
+  font-size: x-large;
+}
+</style>
--- vue-rangefinder/src/pages/MainPage.vue
+++ vue-rangefinder/src/pages/MainPage.vue
@@ -1,16 +1,55 @@
-<template>
-    <div>
-      <h1>PUBG RANGEFINDER Welcome Page</h1>
-    </div>
-  </template>
-  
 <script>
-export default {
+import MapCard from "@/components/MapCard.vue";
+import { getThumbnails } from '@/libs/mapsParams';
+import bgImg from '@/assets/img/pubg-bg.jpg';
 
+export default {
+  data() {
+    return {
+      bgimg: `url(${bgImg})`,
+      maps: getThumbnails(),
+    };
+  },
+  components: {
+    MapCard
+  }
 }
 </script>
-  
-<style scoped>
 
+<template>
+
+<div class="main">
+  <div class="main_cards">
+    <map-card 
+      v-for="map in maps" :key="map" 
+        :map-img="map.thumb" 
+        :map-name="map.name"
+      @click="$router.push(`/${map.name.toLowerCase()}`)"
+    />
+  </div>
+</div>
+
+</template>
+
+<style scoped>
+.main{
+  min-height: calc(100vh - 50px);
+  background-color: black;
+  background-image: v-bind(bgimg);
+  background-size: cover;
+  filter: brightness(0.9);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.main_cards{
+  width: 90%;
+  margin-top: 25%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  flex-wrap: wrap;
+}
 </style>
-  
